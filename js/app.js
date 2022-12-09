8ce1d4e92f715ea9f75ebce3cc5d273c77c4caee
WrJamshid <wr.jamshid@gmail.com>
2022-12-10 00:24:44 +0500

    js test

---
+++ js/app.js
@@ -0,0 +1,135 @@
+// let user = prompt("YOSHINGIZNI KRITING");
+// if (+user) {
+//   console.log("YOSHINGIZ QABUL QILINDI");
+// } else {
+//   console.log("ILTIMOS Son Kriting!");
+// }
+//=================================1-MASALA NUMBER AND STRING!
+
+// let num1 = prompt("birinchi sonni kriting");
+// let num2 = prompt("ikkinchi sonni kriting");
+// if (num1 > num2) {
+//   console.log("katta son", num1);
+// } else if (num1 == num2) {
+//   console.log("bir xil son krittingiz!!!");
+// } else if (num1 < num2) {
+//   console.log("katta son", num2);
+// }
+//==================================2-masala birinchi va ikkinchi son
+
+let score = prompt("iltimos to'plagan ballingizni kriting!");
+if (+score == 100) {
+  console.log("grant");
+} else if (+score == 99) {
+  console.log("grant");
+} else if (+score == 98) {
+  console.log("grant");
+} else if (+score == 97) {
+  console.log("grant");
+} else if (+score == 96) {
+  console.log("grant");
+} else if (+score == 95) {
+  console.log("grant");
+} else if (+score == 94) {
+  console.log("grant");
+} else if (+score == 93) {
+  console.log("grant");
+} else if (+score == 92) {
+  console.log("grant");
+} else if (+score == 91) {
+  console.log("grant");
+} else if (+score == 90) {
+  console.log("grant");
+}
+// =============================GRANT
+else if (+score == 89) {
+  console.log("kontrakt");
+} else if (+score == 88) {
+  console.log("kontrakt");
+} else if (+score == 87) {
+  console.log("kontrakt");
+} else if (+score == 86) {
+  console.log("kontrakt");
+} else if (+score == 85) {
+  console.log("kontrakt");
+} else if (+score == 84) {
+  console.log("kontrakt");
+} else if (+score == 83) {
+  console.log("kontrakt");
+} else if (+score == 82) {
+  console.log("kontrakt");
+} else if (+score == 81) {
+  console.log("kontrakt");
+} else if (+score == 80) {
+  console.log("kontrakt");
+} else if (+score == 79) {
+  console.log("kontrakt");
+} else if (+score == 78) {
+  console.log("kontrakt");
+} else if (+score == 77) {
+  console.log("kontrakt");
+} else if (+score == 76) {
+  console.log("kontrakt");
+} else if (+score == 75) {
+  console.log("kontrakt");
+} else if (+score == 74) {
+  console.log("kontrakt");
+} else if (+score == 73) {
+  console.log("kontrakt");
+} else if (+score == 72) {
+  console.log("kontrakt");
+} else if (+score == 71) {
+  console.log("kontrakt");
+} else if (+score == 70) {
+  console.log("kontrakt");
+}
+// ==============================KONTRAKT
+else if (+score == 69) {
+  console.log("super kontrakt");
+} else if (+score == 68) {
+  console.log("super kontrakt");
+} else if (+score == 67) {
+  console.log("super kontrakt");
+} else if (+score == 66) {
+  console.log("super kontrakt");
+} else if (+score == 65) {
+  console.log("super kontrakt");
+} else if (+score == 64) {
+  console.log("super kontrakt");
+} else if (+score == 63) {
+  console.log("super kontrakt");
+} else if (+score == 62) {
+  console.log("super kontrakt");
+} else if (+score == 61) {
+  console.log("super kontrakt");
+} else if (+score == 60) {
+  console.log("super kontrakt");
+} else if (+score == 59) {
+  console.log("super kontrakt");
+} else if (+score == 58) {
+  console.log("super kontrakt");
+} else if (+score == 57) {
+  console.log("super kontrakt");
+} else if (+score == 56) {
+  console.log("super kontrakt");
+} else if (+score == 55) {
+  console.log("super kontrakt");
+} else if (+score == 54) {
+  console.log("super kontrakt");
+} else if (+score == 53) {
+  console.log("super kontrakt");
+} else if (+score == 52) {
+  console.log("super kontrakt");
+} else if (+score == 51) {
+  console.log("super kontrakt");
+} else if (+score == 50) {
+  console.log("super kontrakt");
+}
+// ===============================SUPER KONTRAKT
+else if (+score < 50) {
+  console.log("kealsi yil urunib ko'ring");
+} else if (+score > 100) {
+  console.log("bunday ball mavjud emas");
+} else {
+  console.log("ILTIMOS Son Kriting!");
+}
